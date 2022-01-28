6c0c48499d8f96657f5b126839b2ee3bedfc69cc
gcerv93 <gcerv93@protonmail.com>
2022-01-28 12:45:55 -0600

    add display_board method

--- lib/board.rb
+++ lib/board.rb
@@ -8,12 +8,34 @@ class Board
     @board = Array.new(6) { Array.new(7, '') }
   end
 
+  def display_board
+    board.each do |row|
+      row.each_with_index do |cell, i|
+        row[i] = ' ' if cell.empty?
+      end
+      puts "| #{row[0]} | #{row[1]} | #{row[2]} | #{row[3]} | #{row[4]} | #{row[5]} | #{row[6]} |"
+    end
+    puts '-----------------------------'
+    puts '| 1 | 2 | 3 | 4 | 5 | 6 | 7 |'
+    reconfigure_board
+  end
+
   def update_board(column, symbol)
     board.each_with_index do |row, idx|
       if row[column].empty? && idx == 5
         row[column] = symbol
       elsif !row[column].empty? && board[idx - 1][column].empty?
         board[idx - 1][column] = symbol
+      end
+    end
+  end
+
+  private
+
+  def reconfigure_board
+    board.each do |row|
+      row.each_with_index do |cell, i|
+        row[i] = '' if cell == ' '
       end
     end
   end
